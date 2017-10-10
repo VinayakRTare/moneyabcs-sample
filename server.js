acf28ebf02bbd855bc88dc85f08a50790906cf00
VinayakRTare <vinayakravindra.tare@mavs.uta.edu>
2017-10-10 18:00:28 -0500

    mongoose.connect

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var initDb = function(callback) {
   var mongodb = require('mongodb');
   if (mongodb == null) return;
 
-  mongodb.connect(mongoURL, function(err, conn) {
+  mongoose.connect(mongoURL, function(err, conn) {
     if (err) {
       callback(err);
       return;
